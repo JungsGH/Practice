4063710fa2545da42e8d214ad0fcc00105fe7be9
dtc1674 <dtc1674@DESKTOP-7SHSBFQ>
2020-11-23 01:23:00 +0900

    2020.11.23

--- src/project/practice_8.java
+++ src/project/practice_8.java
@@ -1,4 +1,4 @@
-/* 벡준 3052 나머지
+/* 백준 3052 나머지
  * 두 자연수 A와 B가 있을 때, A%B는 A를 B로 나눈 나머지 이다. 
  * 예를 들어, 7, 14, 27, 38을 3으로 나눈 나머지는 1, 2, 0, 2이다. 
  * 수 10개를 입력받은 뒤, 이를 42로 나눈 나머지를 구한다.
@@ -9,12 +9,29 @@
  * 출력 : 6
  * 
  * */
+
 package project;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class practice_8 {
-
-	public static void main(String[] args) {
-
-	}
+		
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			
+			ArrayList<Integer> list = new ArrayList<Integer>();
+			
+			for(int i = 0; i < 10; i++) {
+				int a = (sc.nextInt()) % 42;
+				if(list.indexOf(a) < 0) {
+					list.add(a);
+					
+				}
+			}
+			
+			System.out.println(list.size());
+			
+		}
 
 }
